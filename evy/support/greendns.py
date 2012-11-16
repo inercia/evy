e43e8094859fa15f98363a1c8fc2d0921b90c8f3
Alvaro <saurin@tid.es>
2012-11-16 09:53:31 +0100

    Minor: imports

--- evy/support/greendns.py
+++ evy/support/greendns.py
@@ -35,6 +35,9 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import sys
+import struct
+
 from evy import patcher
 from evy.green import _socket_nodns
 from evy.green import time
