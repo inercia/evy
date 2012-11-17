1f0fc467e1de523c5fa6fa6e04cd67c19b0f1300
Alvaro Saurin <saurin@tid.es>
2012-11-17 12:17:57 +0100

    Merge of tests

--- tests/test_event.py
+++ tests/test_event.py
@@ -72,5 +72,87 @@ class TestEvent(LimitedTestCase):
             assert result is X, 'Nobody sent anything to event2 yet it received %r' % (result, )
 
 
+class TestWaiters(LimitedTestCase):
+    def test_waiting_for_event (self):
+        evt = Event()
+        value = 'some stuff'
+
+        def send_to_event ():
+            evt.send(value)
+
+        evy.spawn_n(send_to_event)
+        self.assertEqual(evt.wait(), value)
+
+    def test_multiple_waiters (self):
+        self._test_multiple_waiters(False)
+
+    def test_multiple_waiters_with_exception (self):
+        self._test_multiple_waiters(True)
+
+    def _test_multiple_waiters (self, exception):
+        evt = Event()
+        value = 'some stuff'
+        results = []
+
+        def wait_on_event (i_am_done):
+            evt.wait()
+            results.append(True)
+            i_am_done.send()
+            if exception:
+                raise Exception()
+
+        waiters = []
+        count = 5
+        for i in range(count):
+            waiters.append(Event())
+            evy.spawn_n(wait_on_event, waiters[-1])
+        evy.sleep()  # allow spawns to start executing
+        evt.send()
+
+        for w in waiters:
+            w.wait()
+
+        self.assertEqual(len(results), count)
+
+    def test_reset (self):
+        evt = Event()
+
+        # calling reset before send should throw
+        self.assertRaises(AssertionError, evt.reset)
+
+        value = 'some stuff'
+
+        def send_to_event ():
+            evt.send(value)
+
+        evy.spawn_n(send_to_event)
+        self.assertEqual(evt.wait(), value)
+
+        # now try it again, and we should get the same exact value,
+        # and we shouldn't be allowed to resend without resetting
+        value2 = 'second stuff'
+        self.assertRaises(AssertionError, evt.send, value2)
+        self.assertEqual(evt.wait(), value)
+
+        # reset and everything should be happy
+        evt.reset()
+
+        def send_to_event2 ():
+            evt.send(value2)
+
+        evy.spawn_n(send_to_event2)
+        self.assertEqual(evt.wait(), value2)
+
+    def test_double_exception (self):
+        evt = Event()
+        # send an exception through the event
+        evt.send(exc = RuntimeError('from test_double_exception'))
+        self.assertRaises(RuntimeError, evt.wait)
+        evt.reset()
+        # shouldn't see the RuntimeError again
+        evy.Timeout(0.001)
+        self.assertRaises(evy.Timeout, evt.wait)
+
+
 if __name__ == '__main__':
     unittest.main()
