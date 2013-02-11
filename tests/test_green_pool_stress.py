691d9efa63adcd3b0f17319a219d02cc6d133187
Alvaro Saurin <saurin@tid.es>
2013-02-11 17:14:57 +0100

    Use a Nose attribute for performance tests

--- tests/test_green_pool_stress.py
+++ tests/test_green_pool_stress.py
@@ -98,9 +98,11 @@ class Stress(tests.LimitedTestCase):
         for l in latest[1:]:
             self.assertEquals(l, iters - 1)
 
+    @tests.attr('perf')
     def test_ordering_5 (self):
         self.spawn_order_check(5)
 
+    @tests.attr('perf')
     def test_ordering_50 (self):
         self.spawn_order_check(50)
 
@@ -131,17 +133,19 @@ class Stress(tests.LimitedTestCase):
             # make sure we got to the end
         self.assertEquals(latest, count - 1)
 
-    #@tests.skip_unless(os.environ.get('RUN_STRESS_TESTS') == 'YES')
+    @tests.attr('perf')
     def test_imap_50 (self):
         self.imap_memory_check(50)
 
+    @tests.attr('perf')
     def test_imap_500 (self):
         self.imap_memory_check(500)
 
+    @tests.attr('perf')
     def test_with_intpool (self):
-        from evy import pools
+        from evy.pools import Pool
 
-        class IntPool(pools.Pool):
+        class IntPool(Pool):
             def create (self):
                 self.current_integer = getattr(self, 'current_integer', 0) + 1
                 return self.current_integer
